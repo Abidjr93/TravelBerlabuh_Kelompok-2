96c11c96ef81b4e32002b4a90105c6dece8ff7eb
AddienGM <uclitpenjahat3@gmail.com>
2025-04-22 00:16:05 +0700

    addin1

--- application/views/layout/home.php
+++ application/views/layout/home.php
@@ -1,13 +1,11 @@
-
-<section class="hero">
-        <div class="container">
-            <div class="hero-content">
-                <h1>Discover the World</h1>
-                <p>Explore amazing destinations with our curated travel experiences</p>
-                <a href="<?php echo base_url('destinasi');?>" class="btn">View Tours</a>
-            </div>
-        </div>
-    </section>
-<H1>Ini Adalah Tampilan Home Brody</H1>
-<p>Tambah Satu</p>
-  
+<div class="container-motto">
+    <div class="container-motto-slider">
+        <div class="container-motto-slide" style="background-image: url('assets/images/Bromo1.jpg');"></div>
+        <div class="container-motto-slide" style="background-image: url('assets/images/Ijen1.jpg');"></div>
+        <div class="container-motto-slide" style="background-image: url('assets/images/Dieng1.jpg');"></div>
+    </div>
+    <div class="container-motto-text">
+        <h1>Discover the World</h1>
+        <p>Explore amazing destinations with our curated travel experiences</p>
+    </div>
+</div>
